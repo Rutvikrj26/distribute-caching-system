38b23a440bf491b9a3aef65d6b3e0ddc319e3da1
Rutvik Solanki <rutvikrj26@gmail.com>
2023-03-11 21:27:06 -0500

    Instance IPs added

--- config.py
+++ config.py
@@ -22,18 +22,18 @@ class Config(object):
     MAX_NODES = 8
 
     # IP ADDRESSES
-    FRONTEND_URL = "http://127.0.0.1:5000/"
-    MANAGER_APP_URL = "http://127.0.0.1:5001/"
-    AUTOSCALER_APP_URL = "http://127.0.0.1:5002/"
-    S3_APP_URL = "http://127.0.0.1:5003/"
-    MEMAPP_0_URL = "http://127.0.0.1:5004/"
-    MEMAPP_1_URL = "http://127.0.0.1:5005/"
-    MEMAPP_2_URL = "http://127.0.0.1:5006/"
-    MEMAPP_3_URL = "http://127.0.0.1:5007/"
-    MEMAPP_4_URL = "http://127.0.0.1:5008/"
-    MEMAPP_5_URL = "http://127.0.0.1:5009/"
-    MEMAPP_6_URL = "http://127.0.0.1:5010/"
-    MEMAPP_7_URL = "http://127.0.0.1:5011/"
+    FRONTEND_URL = "http://3.139.103.194:5000/"
+    MANAGER_APP_URL = "http://3.139.103.194:5001/"
+    AUTOSCALER_APP_URL = "http://3.139.103.194:5002/"
+    S3_APP_URL = "http://3.139.103.194:5003//"
+    MEMAPP_0_URL = "http://3.145.153.213:5000/"
+    MEMAPP_1_URL = "http://3.145.7.90:5000/"
+    MEMAPP_2_URL = "http://3.142.135.104:5000/"
+    MEMAPP_3_URL = "http://3.12.107.84:5000/"
+    MEMAPP_4_URL = "http://18.118.207.236:5000/"
+    MEMAPP_5_URL = "http://3.15.223.27:5000/"
+    MEMAPP_6_URL = "http://18.188.246.106:5000/"
+    MEMAPP_7_URL = "http://18.224.69.57:5000/"
 
     # Cloudwatch metrics - use these, so we don't accidentally make doubles that cost $$$
     misses = "misses"
