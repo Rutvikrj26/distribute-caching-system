5fc7f82ff06732c21c6828ebe3f45bc64da64b16
Rutvik Solanki <rutvikrj26@gmail.com>
2023-03-13 00:11:36 -0400

    Switched the Memcache Configuraton Page.

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -13,14 +13,3 @@ migrate = Migrate(frontend, db)
 bootstrap = Bootstrap(frontend)
 
 from frontend import routes, models
-from frontend.models import MemcacheConfig
-
-with frontend.app_context():
-    db.create_all()
-    current_memcache_configs = MemcacheConfig.query.all()
-    for config in current_memcache_configs:
-        db.session.delete(config)
-    db.session.commit()
-    initial_memcache_config = MemcacheConfig(id=1, isRandom=1, maxSize=2)
-    db.session.add(initial_memcache_config)
-    db.session.commit()
--- manager_app/__init__.py
+++ manager_app/__init__.py
@@ -14,7 +14,17 @@ bootstrap = Bootstrap(manager_app)
 from manager_app import routes, models
 
 with manager_app.app_context():
+    # creating the tables in the database
     db.create_all()
+    # Creating the default config
     default_config = models.ManagerConfig()
+    current_memcache_configs = models.MemcacheConfig.query.all()
+    for config in current_memcache_configs:
+        db.session.delete(config)
+    db.session.commit()
+    initial_memcache_config = models.MemcacheConfig(id=1, isRandom=1, maxSize=2)
+    # adding the default config
     db.session.add(default_config)
+    db.session.add(initial_memcache_config)
+    # commiting the changes
     db.session.commit()
